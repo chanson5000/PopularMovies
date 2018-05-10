752ff455bc1043c42a4e2a3abe99468cfa37f504
Cory <chanson5000@gmail.com>
2018-05-10 16:07:46 -0500

    Added some network functionality.

---
+++ app/src/main/java/com/nverno/popularmovies/util/JsonParse.java
@@ -0,0 +1,32 @@
+package com.nverno.popularmovies.util;
+
+import com.nverno.popularmovies.model.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonParse {
+    public static List<Movie> topRatedResults(String jsonString) throws JSONException {
+        JSONObject jsonMoviesObject = new JSONObject(jsonString);
+        JSONArray moviesResultsArray = jsonMoviesObject.getJSONArray("results");
+        List<Movie> movies = new ArrayList<>();
+        int len = moviesResultsArray.length();
+        for (int i=0; i<len; i++)
+        {
+            JSONObject movie = moviesResultsArray.getJSONObject(i);
+
+            movies.add(new Movie(movie.getInt("id"),
+                    movie.getString("title"),
+                    movie.getString("poster_path"),
+                    movie.getString("overview"),
+                    movie.getDouble("vote_average"),
+                    movie.getString("release_date")));
+        }
+
+        return movies;
+    }
+}
