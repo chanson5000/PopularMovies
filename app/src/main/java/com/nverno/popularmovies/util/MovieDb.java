2a93dd10a256daa25f171612c96980a057d5e457
Cory <chanson5000@gmail.com>
2018-05-11 18:11:56 -0500

    Added a URI builder.

--- app/src/main/java/com/nverno/popularmovies/util/MovieDb.java
+++ app/src/main/java/com/nverno/popularmovies/util/MovieDb.java
@@ -4,6 +4,7 @@ import android.net.Uri;
 
 import com.nverno.popularmovies.model.Movie;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
@@ -15,10 +16,20 @@ public class MovieDb implements MovieDbApi {
     final static String PAGE_ONE = "&page=1";
     final static String AUTH = AUTH_STRING + API_KEY;
 
+    final static String API_PARAM = "api_key";
+    final static String PAGE_PARAM = "page";
+
     public static List<Movie> getTopRated() {
+
+        Uri requestUri = Uri.parse(BASE_URL + TOP_RATED).buildUpon()
+                .appendQueryParameter(API_PARAM, API_KEY)
+                .appendQueryParameter(PAGE_PARAM, Integer.toString(1))
+                .build();
+
+        URL requestUrl = Network.makeUrl(requestUri);
+
         try {
-            URL url = Network.makeUrl(BASE_URL + TOP_RATED + AUTH + PAGE_ONE);
-            String jsonResponseString = Network.fetchHttp(url);
+            String jsonResponseString = Network.fetchHttpsResponse(requestUrl);
 
             return JsonParse.topRatedResults(jsonResponseString);
 
@@ -28,17 +39,7 @@ public class MovieDb implements MovieDbApi {
         return null;
     }
 
-
     public static URL getTopRatedUrl() {
         return Network.makeUrl(BASE_URL + TOP_RATED + AUTH + PAGE_ONE);
     }
-
-    public static Uri getTopRatedUri() {
-        String base = BASE_URL + TOP_RATED + AUTH + PAGE_ONE;
-
-            Uri uri = Uri.parse(base).buildUpon().build();
-
-            return uri;
-    }
-
 }
