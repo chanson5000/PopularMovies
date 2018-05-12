7cdca241cb2e5f502334ba890387e9abe7fc1670
Cory <chanson5000@gmail.com>
2018-05-12 16:04:21 -0500

    Some code formatting and comments.

--- app/src/main/java/com/nverno/popularmovies/MainActivity.java
+++ app/src/main/java/com/nverno/popularmovies/MainActivity.java
@@ -11,12 +11,12 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
+
 import com.nverno.popularmovies.model.Movie;
 import com.nverno.popularmovies.util.JsonParse;
 import com.nverno.popularmovies.util.MovieDb;
 import com.nverno.popularmovies.util.Network;
+
 import java.net.URL;
 import java.util.List;
 
@@ -29,9 +29,22 @@ public class MainActivity extends AppCompatActivity implements
 
     private PosterAdapter mPosterAdapter;
 
-    @BindView(R.id.recycler_view_posters) RecyclerView mRecyclerView;
+    @BindView(R.id.recycler_view_posters) private RecyclerView mRecyclerView;
 
+    LoaderManager.LoaderCallbacks<List<Movie>> mCallback;
+
+    // To identify our loader type. (only using one, anyway)
     private static final int POSTER_LOADER = 0;
+
+    // To identify our sort types.
+    private static final int SORT_MOST_POPULAR = 0;
+    private static final int SORT_TOP_RATED = 1;
+
+    // A persistent way to keep track of our sort type.
+    private static int sMovieSortType = SORT_MOST_POPULAR;
+
+    // To identify that we are moving movie data through an intent.
+    private static final String INTENT_MOVIE_DATA = "MOVIE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +53,8 @@ public class MainActivity extends AppCompatActivity implements
 
         ButterKnife.bind(this);
 
-        GridLayoutManager layoutManager =
-                new GridLayoutManager(this, 2);
+        // Using the GridLayoutManager with 2 columns.
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
 
         // Associate the LayoutManager with the RecyclerView.
         mRecyclerView.setLayoutManager(layoutManager);
@@ -55,17 +68,8 @@ public class MainActivity extends AppCompatActivity implements
         // Set the adapter, attaching it to the RecyclerView in the layout.
         mRecyclerView.setAdapter(mPosterAdapter);
 
-        int loaderId = POSTER_LOADER;
-
-        LoaderManager.LoaderCallbacks<List<Movie>> callback = MainActivity.this;
-        // TODO: Implement showLoading();
-        // showLoading();
-
-        // Ensure the loader is initialized. One is created if doesn't exist, else last is used.
-        // TODO: Implement this correctly.
-
-
-        getSupportLoaderManager().initLoader(loaderId, null, callback);
+        mCallback = MainActivity.this;
+        getSupportLoaderManager().initLoader(POSTER_LOADER, null, mCallback);
 
     }
 
@@ -87,13 +91,19 @@ public class MainActivity extends AppCompatActivity implements
 
             @Override
             public List<Movie> loadInBackground() {
-                    URL requestUrl = MovieDb.getTopRatedUrl();
-                    try {
-                        String jsonResponse = Network.fetchHttpsResponse(requestUrl);
+                URL requestUrl;
+                if (sMovieSortType == SORT_TOP_RATED) {
+                    requestUrl = MovieDb.getTopRatedUrl();
+                } else {
+                    requestUrl = MovieDb.getPopularUrl();
+                }
 
-                        List<Movie> movies = JsonParse.topRatedResults(jsonResponse);
+                try {
+                    String jsonResponse = Network.fetchHttpsResponse(requestUrl);
 
-                        return movies;
+                    List<Movie> movies = JsonParse.topRatedResults(jsonResponse);
+
+                    return movies;
                 } catch (Exception e) {
                     e.printStackTrace();
                     return null;
@@ -107,11 +117,13 @@ public class MainActivity extends AppCompatActivity implements
         };
     }
 
+    // When our AsyncTask finishes loading, we can set the data into the PosterAdapter.
     @Override
     public void onLoadFinished(Loader<List<Movie>> loader, List<Movie> data) {
         mPosterAdapter.setPosterData(data);
     }
 
+    // I don't think I needed to use this yet, but I have to override it...
     @Override
     public void onLoaderReset(Loader<List<Movie>> loader) {
 
@@ -120,32 +132,55 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     public void onClick(Movie movieForDay) {
         Context context = this;
+
+        // Where this click intent is going...
         Class destinationClass = DetailActivity.class;
         Intent intentToStartDetailActivity = new Intent(context, destinationClass);
 
-        intentToStartDetailActivity.putExtra("MOVIE", movieForDay);
+        // Pushing our parcelable movie data through the intent.
+        intentToStartDetailActivity.putExtra(INTENT_MOVIE_DATA, movieForDay);
+
         startActivity(intentToStartDetailActivity);
     }
 
+    // Create and inflate the options menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
+
+        // Sort by most popular is default and checked.
+        menu.findItem(R.id.sort_by_most_popular).setChecked(true);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int menuItemThatWasSelected = item.getItemId();
-        if (menuItemThatWasSelected == R.id.action_settings) {
-            Context context = MainActivity.this;
-            String message = "Settings clicked";
-            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+        switch (item.getItemId()) {
+            // If sort by most popular has been clicked...
+            case R.id.sort_by_most_popular:
+                // and the sort type is not already "most popular"
+                if (sMovieSortType != SORT_MOST_POPULAR) {
+                    // set the sort type to "most popular"
+                    sMovieSortType = SORT_MOST_POPULAR;
+                    // set that item as checked.
+                    item.setChecked(true);
+                    // restart the loader to refresh the poster sort type
+                    getSupportLoaderManager().restartLoader(POSTER_LOADER, null, mCallback);
+                }
+                return true;
 
+                // Every thing like above, but the opposite.
+            case R.id.sort_by_top_rated:
+                if (sMovieSortType != SORT_TOP_RATED) {
+                    sMovieSortType = SORT_TOP_RATED;
+                    item.setChecked(true);
+                    getSupportLoaderManager().restartLoader(POSTER_LOADER, null, mCallback);
+                }
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void showPosterDataView() {
-        mRecyclerView.setVisibility(View.VISIBLE);
     }
 }
