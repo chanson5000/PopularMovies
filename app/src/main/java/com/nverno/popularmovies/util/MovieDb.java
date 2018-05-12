a0e4b6bb0308f0c76b9b7e7134b34ab58814b2f6
Cory <chanson5000@gmail.com>
2018-05-12 16:09:02 -0500

    Code reformat with comments. Set some variables private.

--- app/src/main/java/com/nverno/popularmovies/util/MovieDb.java
+++ app/src/main/java/com/nverno/popularmovies/util/MovieDb.java
@@ -10,17 +10,27 @@ import java.util.List;
 
 public class MovieDb implements MovieDbApi {
 
-    final static String BASE_URL = "https://api.themoviedb.org/3/";
-    final static String TOP_RATED = "movie/top_rated";
-    final static String AUTH_STRING = "?api_key=";
-    final static String PAGE_ONE = "&page=1";
-    final static String AUTH = AUTH_STRING + API_KEY;
+    private final static String BASE_URL = "https://api.themoviedb.org/3/";
+    private final static String TOP_RATED = "movie/top_rated";
+    private final static String POPULAR = "movie/popular";
+    private final static String AUTH_STRING = "?api_key=";
+    private final static String PAGE_ONE = "&page=1";
+    private final static String AUTH = AUTH_STRING + API_KEY;
 
-    final static String API_PARAM = "api_key";
-    final static String PAGE_PARAM = "page";
+    private final static String API_PARAM = "api_key";
+    private final static String PAGE_PARAM = "page";
 
+    // I only really need these two for now...
+    public static URL getTopRatedUrl() {
+        return Network.makeUrl(BASE_URL + TOP_RATED + AUTH + PAGE_ONE);
+    }
+
+    public static URL getPopularUrl() {
+        return Network.makeUrl(BASE_URL + POPULAR + AUTH + PAGE_ONE);
+    }
+
+    // I may want to implement this URI builder in the future...
     public static List<Movie> getTopRated() {
-
         Uri requestUri = Uri.parse(BASE_URL + TOP_RATED).buildUpon()
                 .appendQueryParameter(API_PARAM, API_KEY)
                 .appendQueryParameter(PAGE_PARAM, Integer.toString(1))
@@ -37,9 +47,5 @@ public class MovieDb implements MovieDbApi {
             e.printStackTrace();
         }
         return null;
-    }
-
-    public static URL getTopRatedUrl() {
-        return Network.makeUrl(BASE_URL + TOP_RATED + AUTH + PAGE_ONE);
     }
 }
