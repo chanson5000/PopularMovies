567c1e91ff2510d2ea241e02a0557f73c734e7d0
Cory <chanson5000@gmail.com>
2018-06-12 17:18:20 -0500

    Added Movie database abstraction.

---
+++ app/src/main/java/com/nverno/popularmovies/database/MovieDatabase.java
@@ -0,0 +1,34 @@
+package com.nverno.popularmovies.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import android.util.Log;
+
+import com.nverno.popularmovies.model.Movie;
+
+@Database(entities = {Movie.class}, version = 1, exportSchema = false)
+public abstract class MovieDatabase extends RoomDatabase {
+
+    private static final String LOG_TAG = MovieDatabase.class.getSimpleName();
+    private static final Object LOCK = new Object();
+    private static final String DATABASE_NAME = "moviedbdata";
+    private static MovieDatabase sInstance;
+
+    public static MovieDatabase getsInstance(Context context) {
+        // If a database does not exist, create one.
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                Log.d(LOG_TAG, "Creating a new database instance");
+                sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                        MovieDatabase.class, MovieDatabase.DATABASE_NAME)
+                        .build();
+            }
+        }
+        Log.d(LOG_TAG, "Getting the database instance");
+        return sInstance;
+    }
+
+    public abstract MovieDao movieDao();
+}
