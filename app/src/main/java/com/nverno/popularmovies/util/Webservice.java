04662d54aa4f65ab31f95a8afe8d0bc9e2487653
Cory <chanson5000@gmail.com>
2018-06-13 14:14:32 -0500

    Temp fetch web data.

--- app/src/main/java/com/nverno/popularmovies/util/Webservice.java
+++ app/src/main/java/com/nverno/popularmovies/util/Webservice.java
@@ -1,15 +1,11 @@
 package com.nverno.popularmovies.util;
 
 import com.nverno.popularmovies.model.Movie;
-import com.nverno.popularmovies.repository.MovieDbApi;
 
 import java.util.List;
 
 import retrofit2.Call;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
-import retrofit2.http.Query;
 
 public interface Webservice {
 
