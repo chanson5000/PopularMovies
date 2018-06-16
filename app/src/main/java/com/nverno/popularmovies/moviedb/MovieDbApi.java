33c83ea0bf4e2c782f0375724478e323eef48dc4
Cory <chanson5000@gmail.com>
2018-06-16 18:57:48 -0500

    Of course an interface can't extend another interface.

--- app/src/main/java/com/nverno/popularmovies/moviedb/MovieDbApi.java
+++ app/src/main/java/com/nverno/popularmovies/moviedb/MovieDbApi.java
@@ -5,7 +5,7 @@ import com.nverno.popularmovies.model.Results;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
-public interface MovieDbApi extends MovieDbSecrets {
+public interface MovieDbApi {
 
     @GET("3/movie/top_rated?api_key=" + MovieDbSecrets.API_KEY + "&page=1")
     Call<Results> topRatedMovies();
