d3d4a99ea3c7ebc4b2ac080bfa8a994d19e1580d
Cory <chanson5000@gmail.com>
2018-06-16 18:43:38 -0500

    Refactored and moved our movie db api interface.

---
+++ app/src/main/java/com/nverno/popularmovies/moviedb/MovieDbApi.java
@@ -0,0 +1,16 @@
+package com.nverno.popularmovies.moviedb;
+
+import com.nverno.popularmovies.model.Results;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface MovieDbApi extends MovieDbSecrets {
+
+    @GET("3/movie/top_rated?api_key=" + MovieDbSecrets.API_KEY + "&page=1")
+    Call<Results> topRatedMovies();
+
+    @GET("3/movie/popular?api_key=" + MovieDbSecrets.API_KEY + "&page=1")
+    Call<Results> popularMovies();
+
+}
