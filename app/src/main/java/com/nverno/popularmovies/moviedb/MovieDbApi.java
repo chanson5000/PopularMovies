d2107778c1dbf72484339636622945a0ed4730ee
Cory <chanson5000@gmail.com>
2018-06-16 21:35:44 -0500

    Added (so far correct) api calls.

--- app/src/main/java/com/nverno/popularmovies/moviedb/MovieDbApi.java
+++ app/src/main/java/com/nverno/popularmovies/moviedb/MovieDbApi.java
@@ -1,6 +1,8 @@
 package com.nverno.popularmovies.moviedb;
 
 import com.nverno.popularmovies.model.MovieResult;
+import com.nverno.popularmovies.model.ReviewResult;
+import com.nverno.popularmovies.model.TrailerResult;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -14,8 +16,13 @@ public interface MovieDbApi {
     @GET("3/movie/popular?api_key=" + MovieDbSecrets.API_KEY + "&page=1")
     Call<MovieResult> popularMovies();
 
-    @GET("2/movie/{movie_id}/reviews?api_key=" + MovieDbSecrets.API_KEY + "&page=1")
-    Call<MovieResult> reviews(
+    @GET("3/movie/{movie_id}/reviews?api_key=" + MovieDbSecrets.API_KEY + "&page=1")
+    Call<ReviewResult> reviews(
+            @Path("movie_id") int movieId
+    );
+
+    @GET("3/movie/{movie_id}/videos?api_key=" + MovieDbSecrets.API_KEY + "&page=1")
+    Call<TrailerResult> trailers(
             @Path("movie_id") int movieId
     );
 }
