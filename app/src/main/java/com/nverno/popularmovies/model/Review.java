8f2d1e7659bcde7dcd7aaa757b4ba4087a2cbd71
Cory <chanson5000@gmail.com>
2018-06-16 21:26:50 -0500

    Added review class.

---
+++ app/src/main/java/com/nverno/popularmovies/model/Review.java
@@ -0,0 +1,21 @@
+package com.nverno.popularmovies.model;
+
+import com.google.gson.annotations.Expose;
+
+public class Review {
+
+    @Expose
+    private String id;
+    @Expose
+    private String author;
+    @Expose
+    private String content;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getReview() {
+        return content;
+    }
+}
