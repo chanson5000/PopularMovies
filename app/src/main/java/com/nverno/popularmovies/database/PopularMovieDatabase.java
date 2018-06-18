6ae6e354d823df3bf0e961fe328f8ca45f709992
Cory <chanson5000@gmail.com>
2018-06-18 16:03:46 -0500

    Added Popular Movie database.

---
+++ app/src/main/java/com/nverno/popularmovies/database/PopularMovieDatabase.java
@@ -0,0 +1,33 @@
+package com.nverno.popularmovies.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import android.util.Log;
+
+import com.nverno.popularmovies.model.Movie;
+
+@Database(entities = {Movie.class}, version = 1, exportSchema = false)
+public abstract class PopularMovieDatabase extends RoomDatabase {
+
+    private static final String LOG_TAG = PopularMovieDatabase.class.getSimpleName();
+    private static final Object LOCK = new Object();
+    private static final String DATABASE_NAME = "popularmovies";
+    private static PopularMovieDatabase sInstance;
+
+    public static PopularMovieDatabase getInstance(Context context) {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                Log.d(LOG_TAG, "Creating new Popular Movies database instance");
+                sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                        PopularMovieDatabase.class, PopularMovieDatabase.DATABASE_NAME)
+                        .build();
+            }
+        }
+        Log.d(LOG_TAG, "Getting Popular Movies database instance");
+        return sInstance;
+    }
+
+    public abstract MovieDao movieDao();
+}
