d237bf2822a0fd6f3089c143445610fe0e4c0684
Cory <chanson5000@gmail.com>
2018-06-18 16:04:02 -0500

    Added Top Rated Movie database.

---
+++ app/src/main/java/com/nverno/popularmovies/database/TopRatedMovieDatabase.java
@@ -0,0 +1,33 @@
+package com.nverno.popularmovies.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import android.util.Log;
+
+import com.nverno.popularmovies.model.Movie;
+
+@Database(entities = {Movie.class}, version = 1, exportSchema = false)
+public abstract class TopRatedMovieDatabase extends RoomDatabase {
+
+    private static final String LOG_TAG = TopRatedMovieDatabase.class.getSimpleName();
+    private static final Object LOCK = new Object();
+    private static final String DATABASE_NAME = "topratedmovies";
+    private static TopRatedMovieDatabase sInstance;
+
+    public static TopRatedMovieDatabase getsInstance(Context context) {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                Log.d(LOG_TAG, "Creating new Top Rated Movies database instance");
+                sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                        TopRatedMovieDatabase.class, TopRatedMovieDatabase.DATABASE_NAME)
+                        .build();
+            }
+        }
+        Log.d(LOG_TAG, "Getting Top Rated Movies database instance");
+        return sInstance;
+    }
+
+    public abstract MovieDao movieDao();
+}
