711b4ad925084a33c5f05d26d2b8b4a966e61e58
Cory <chanson5000@gmail.com>
2018-06-18 16:09:56 -0500

    Added Popular Movie repository.

---
+++ app/src/main/java/com/nverno/popularmovies/repository/PopularMovieRepository.java
@@ -0,0 +1,56 @@
+package com.nverno.popularmovies.repository;
+
+import com.nverno.popularmovies.database.PopularMovieDatabase;
+import com.nverno.popularmovies.model.Movie;
+import com.nverno.popularmovies.model.MovieResult;
+import com.nverno.popularmovies.moviedb.MovieDbApi;
+import com.nverno.popularmovies.util.AppExecutors;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class PopularMovieRepository {
+
+    private MovieDbApi movieDbApi;
+
+    public PopularMovieRepository() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.themoviedb.org/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        movieDbApi = retrofit.create(MovieDbApi.class);
+    }
+
+    public void loadPopularMovies(final PopularMovieDatabase popularMovieDatabase) {
+
+        Call<MovieResult> call = movieDbApi.popularMovies();
+
+        call.enqueue(new Callback<MovieResult>() {
+            @Override
+            public void onResponse(Call<MovieResult> call, Response<MovieResult> response) {
+                final List<Movie> movies = response.body().GetMovies();
+
+                AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        popularMovieDatabase.movieDao().insertMany(movies);
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Call<MovieResult> call, Throwable t) {
+
+            }
+        });
+
+    }
+
+
+}
