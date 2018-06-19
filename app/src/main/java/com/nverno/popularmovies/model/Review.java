4a9ff78c03d3bed7c702d90530e82b796150e186
Cory <chanson5000@gmail.com>
2018-06-19 10:19:12 -0500

    Modified Review class for Dao.

--- app/src/main/java/com/nverno/popularmovies/model/Review.java
+++ app/src/main/java/com/nverno/popularmovies/model/Review.java
@@ -1,22 +1,70 @@
 package com.nverno.popularmovies.model;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
 import com.google.gson.annotations.Expose;
 
+@Entity(tableName = "review")
 public class Review {
 
+    @PrimaryKey
     @Expose
     private String id;
+
+    @Expose
+    private int movieId;
+
     @Expose
     private String author;
+
     @Expose
     private String content;
+
+    @Expose
+    private String url;
 
     public Review() {
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
 
     public String getAuthor() {
         return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 
     public String getReview() {
