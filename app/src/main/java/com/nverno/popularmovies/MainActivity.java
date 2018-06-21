c95ede5b4f7a1c21d37437a2f013b0356077a447
Cory <chanson5000@gmail.com>
2018-06-21 11:43:02 -0500

    Updated for much better use of Movies View Model. Combine the other three View Models into one.

--- app/src/main/java/com/nverno/popularmovies/MainActivity.java
+++ app/src/main/java/com/nverno/popularmovies/MainActivity.java
@@ -19,10 +19,7 @@ import com.nverno.popularmovies.adapter.PosterAdapter;
 import com.nverno.popularmovies.model.Movie;
 import com.nverno.popularmovies.repository.ReviewRepository;
 import com.nverno.popularmovies.repository.TrailerRepository;
-import com.nverno.popularmovies.viewmodel.FavoriteMoviesViewModel;
 import com.nverno.popularmovies.viewmodel.MoviesViewModel;
-import com.nverno.popularmovies.viewmodel.PopularMoviesViewModel;
-import com.nverno.popularmovies.viewmodel.TopRatedMoviesViewModel;
 
 import java.util.List;
 
@@ -36,10 +33,6 @@ public class MainActivity extends AppCompatActivity implements
 
     private PosterAdapter mPosterAdapter;
 
-    PopularMoviesViewModel popularMoviesViewModel;
-    TopRatedMoviesViewModel topRatedMoviesViewModel;
-    FavoriteMoviesViewModel favoriteMoviesViewModel;
-
     MoviesViewModel moviesViewModel;
 
     @BindView(R.id.recycler_view_posters)
@@ -52,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements
     // To identify our sort types.
     private static final int SORT_POPULAR = 0;
     private static final int SORT_TOP_RATED = 1;
-    private static final int SHOW_FAVORITES = 3;
+    private static final int SHOW_FAVORITES = 2;
 
     // A persistent way to keep track of our sort type.
     private static int sMovieSortType;
@@ -98,6 +91,12 @@ public class MainActivity extends AppCompatActivity implements
             @Override
             public void onChanged(@Nullable List<Movie> movies) {
                 mPosterAdapter.setPosterData(movies);
+                if (movies != null && !movies.isEmpty()) {
+                    hideLoadingIndicator();
+                    hideNoFavorites();
+                } else if (moviesViewModel.isFavoriteMoviesList()) {
+                    showNoFavorites();
+                }
             }
         });
     }
@@ -188,68 +187,15 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     private void setPopularMoviesView() {
-
-        moviesViewModel.getMovies().setValue(moviesViewModel.loadPopularMovies().getValue());
-
-//        moviesViewModel = ViewModelProviders.of(this)
-//                .get(MoviesViewModel.class);
-//
-//        moviesViewModel.getMovies().observe(this, new Observer<List<Movie>>() {
-//            @Override
-//            public void onChanged(@Nullable List<Movie> movies) {
-//                mPosterAdapter.setPosterData(movies);
-//            }
-//        });
-
-//        popularMoviesViewModel = ViewModelProviders.of(this)
-//                .get(PopularMoviesViewModel.class);
-//
-//        popularMoviesViewModel.getPopularMovies().observe(this, new Observer<List<Movie>>() {
-//            @Override
-//            public void onChanged(@Nullable List<Movie> movies) {
-//                mPosterAdapter.setPosterData(movies);
-//
-//                if (movies != null && !movies.isEmpty()) {
-//                    hideLoadingIndicator();
-//                }
-//            }
-//        });
+        moviesViewModel.loadPopularMovies();
     }
 
     private void setTopRatedMoviesView() {
-        topRatedMoviesViewModel = ViewModelProviders.of(this)
-                .get(TopRatedMoviesViewModel.class);
-
-        topRatedMoviesViewModel.getTopRatedMovies().observe(this, new Observer<List<Movie>>() {
-            @Override
-            public void onChanged(@Nullable List<Movie> movies) {
-                mPosterAdapter.setPosterData(movies);
-
-                if (movies != null && !movies.isEmpty()) {
-                    hideLoadingIndicator();
-                }
-            }
-        });
+        moviesViewModel.loadTopRatedMovies();
     }
 
     private void setFavoriteMoviesView() {
-        favoriteMoviesViewModel = ViewModelProviders.of(this)
-                .get(FavoriteMoviesViewModel.class);
-
-        favoriteMoviesViewModel.getFavoriteMovies().observe(this, new Observer<List<Movie>>() {
-
-            @Override
-            public void onChanged(@Nullable List<Movie> movies) {
-                mPosterAdapter.setPosterData(movies);
-
-                if (movies == null || movies.isEmpty()) {
-                    showNoFavorites();
-                } else {
-                    hideNoFavorites();
-                    hideLoadingIndicator();
-                }
-            }
-        });
+        moviesViewModel.loadFavoriteMovies();
     }
 
     private void showLoadingIndicator() {
--- app/src/main/java/com/nverno/popularmovies/viewmodel/MoviesViewModel.java
+++ app/src/main/java/com/nverno/popularmovies/viewmodel/MoviesViewModel.java
@@ -1,10 +1,15 @@
 package com.nverno.popularmovies.viewmodel;
 
 import android.app.Application;
+import android.arch.core.util.Function;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MediatorLiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.Transformations;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.nverno.popularmovies.model.Movie;
 import com.nverno.popularmovies.repository.FavoriteMovieRepository;
@@ -15,45 +20,156 @@ import java.util.List;
 
 public class MoviesViewModel extends AndroidViewModel {
 
-    private PopularMovieRepository popularMovieRepository;
-    private TopRatedMovieRepository topRatedMovieRepository;
-    private FavoriteMovieRepository favoriteMovieRepository;
-
     private LiveData<List<Movie>> popularMovies;
     private LiveData<List<Movie>> topRatedMovies;
     private LiveData<List<Movie>> favoriteMovies;
 
-    private MutableLiveData<List<Movie>> selectedMovies;
-    private MutableLiveData<Movie> selectedMovie;
+    private MediatorLiveData<List<Movie>> selectedMovies;
+    private LiveData<Movie> selectedMovie;
+
+    private boolean isFavoriteMoviesList = false;
+
+    private static final int SORT_POPULAR = 0;
+    private static final int SORT_TOP_RATED = 1;
+    private static final int SHOW_FAVORITES = 2;
 
     public MoviesViewModel(@NonNull Application app) {
         super(app);
 
-        popularMovieRepository = new PopularMovieRepository(this.getApplication());
-        topRatedMovieRepository = new TopRatedMovieRepository(this.getApplication());
-        favoriteMovieRepository = new FavoriteMovieRepository(this.getApplication());
+        PopularMovieRepository popularMovieRepository = new PopularMovieRepository(this.getApplication());
+        TopRatedMovieRepository topRatedMovieRepository = new TopRatedMovieRepository(this.getApplication());
+        FavoriteMovieRepository favoriteMovieRepository = new FavoriteMovieRepository(this.getApplication());
 
         popularMovies = popularMovieRepository.getPopularMoviesSorted();
         topRatedMovies = topRatedMovieRepository.getTopRatedMoviesSorted();
         favoriteMovies = favoriteMovieRepository.getFavoriteMovies();
 
-        selectedMovies = new MutableLiveData<>();
+        selectedMovies = new MediatorLiveData<>();
+        selectedMovie = new MutableLiveData<>();
     }
 
     public MutableLiveData<List<Movie>> getMovies() {
         return selectedMovies;
     }
 
-    public LiveData<List<Movie>> loadPopularMovies() {
-        return popularMovies;
+    private void removeAllSources() {
+        selectedMovies.removeSource(popularMovies);
+        selectedMovies.removeSource(topRatedMovies);
+        selectedMovies.removeSource(favoriteMovies);
     }
 
-//    private void loadTopRatedMovies() {
-//        selectedMovies = topRatedMovies;
-//    }
-//
-//    private void loadFavoriteMovies() {
-//        selectedMovies = favoriteMovies;
-//    }
+    public void loadPopularMovies() {
+        removeAllSources();
 
+        if (isFavoriteMoviesList) {
+            isFavoriteMoviesList = false;
+        }
+
+        selectedMovies.addSource(popularMovies, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(@Nullable List<Movie> movies) {
+                selectedMovies.setValue(movies);
+            }
+        });
+    }
+
+    public void loadTopRatedMovies() {
+        removeAllSources();
+
+        if (isFavoriteMoviesList) {
+            isFavoriteMoviesList = false;
+        }
+
+        selectedMovies.addSource(topRatedMovies, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(@Nullable List<Movie> movies) {
+                selectedMovies.setValue(movies);
+            }
+        });
+    }
+
+    public void loadFavoriteMovies() {
+        removeAllSources();
+
+        if (!isFavoriteMoviesList) {
+            isFavoriteMoviesList = true;
+        }
+
+        selectedMovies.addSource(favoriteMovies, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(@Nullable List<Movie> movies) {
+                selectedMovies.setValue(movies);
+            }
+        });
+    }
+
+    public LiveData<Movie> getSelectedMovie() {
+        return selectedMovie;
+    }
+
+    public boolean isFavoriteMoviesList() {
+        return isFavoriteMoviesList;
+    }
+
+    public LiveData<Boolean> isFavoriteMovie(final int movieId) {
+        return Transformations.map(favoriteMovies, new Function<List<Movie>, Boolean>() {
+            @Override
+            public Boolean apply(List<Movie> movies) {
+                for (Movie movie : movies) {
+                    if (movie.getId() == movieId) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+    public void setSelectedMovie(int sortType, final int movieId) {
+        switch (sortType) {
+            case SORT_POPULAR:
+                selectedMovie = Transformations.map(popularMovies, new Function<List<Movie>, Movie>() {
+                    @Override
+                    public Movie apply(List<Movie> movies) {
+                        for (Movie movie : movies) {
+                            if (movie.getId() == movieId) {
+                                return movie;
+                            }
+                        }
+                        return null;
+                    }
+                });
+                break;
+
+            case SORT_TOP_RATED:
+                selectedMovie = Transformations.map(topRatedMovies, new Function<List<Movie>, Movie>() {
+                    @Override
+                    public Movie apply(List<Movie> movies) {
+                        for (Movie movie : movies) {
+                            if (movie.getId() == movieId) {
+                                return movie;
+                            }
+                        }
+                        return null;
+                    }
+                });
+                break;
+
+            case SHOW_FAVORITES:
+                selectedMovie = Transformations.map(favoriteMovies, new Function<List<Movie>, Movie>() {
+                    @Override
+                    public Movie apply(List<Movie> movies) {
+                        for (Movie movie : movies) {
+                            if (movie.getId() == movieId) {
+                                return movie;
+                            }
+                        }
+                        return null;
+                    }
+                });
+
+            default:
+                break;
+        }
+    }
 }
